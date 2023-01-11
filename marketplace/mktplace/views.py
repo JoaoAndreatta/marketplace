55d97f620091c5f24803d3857d3b2774893e3f70
João Andreatta <joao.martineliandreatta@gmail.com>
2023-01-10 22:47:01 -0300

    Several Implementantions

--- marketplace/mktplace/views.py
+++ marketplace/mktplace/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from store.models import Product
+from category.models import Category
 
 def home(request):
     products = Product.objects.all().filter(is_avaiable = True)
+    category = Category.objects.all()
 
     context = {
         'products': products,
+        'category': category,
     }
 
     return render(request, 'home.html', context)
